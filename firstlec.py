60379f79391fae2b18157eb83d6c47fc94298905
krishna <krishnaqw4141@gmail.com>
2022-04-28 01:07:51 +0530

    dfdfd

--- firstlec.py
+++ firstlec.py
@@ -1,1 +1,13 @@
-print('dfsdf')
+
+cards=[12,10,9,8,7,6,5,4,3,2,1]
+def locate_card(cards,no):
+    if len(cards) < 0 : return -1
+    for index,card in enumerate(cards):
+        if card == no:
+            print('index of card and no on card',index, card)
+            return index+1
+    return -1
+
+
+
+print(locate_card(cards,5))
